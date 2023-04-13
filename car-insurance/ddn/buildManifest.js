fe36b6faee2e52bfbfc28ac92895ebe01676290e
刘永乐 <liuyongle@bytedance.com>
2023-04-14 02:02:45 +0800

    feat: fix bugs

--- car-insurance/ddn/buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/745-b99e4d57a543bbd5.js","static/chunks/pages/index-a8ad7cbad77ee0e8.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
